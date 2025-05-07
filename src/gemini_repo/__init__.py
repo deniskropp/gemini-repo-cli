071e3402991ea516adb429ebbeff5aaaef6ec480
Denis Oliver Kropp <Leichenbegatter@outlook.com>
2025-05-07 22:41:38 +0200

    add DEFAULT_OLLAMA_HOST

--- src/gemini_repo/__init__.py
+++ src/gemini_repo/__init__.py
@@ -1,6 +1,6 @@
 from .base_api import BaseRepoAPI
 from .gemini_api import GeminiRepoAPI, DEFAULT_GEMINI_MODEL
-from .ollama_api import OllamaRepoAPI, DEFAULT_OLLAMA_MODEL
+from .ollama_api import OllamaRepoAPI, DEFAULT_OLLAMA_MODEL, DEFAULT_OLLAMA_HOST
 
 __all__ = [
     "BaseRepoAPI",
@@ -8,4 +8,5 @@ __all__ = [
     "OllamaRepoAPI",
     "DEFAULT_GEMINI_MODEL",
     "DEFAULT_OLLAMA_MODEL",
+    "DEFAULT_OLLAMA_HOST",
 ]
